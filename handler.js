4bf96d7c1d1ad30ccf95af99a7ffbb7959f7a919
Mikheil Margishvili (მიხეილ მარღიშვილი) <m.marghishvili@adjarabet.com>
2024-11-14 20:21:50 +0400

    first

---
+++ handler.js
@@ -0,0 +1,10 @@
+exports.logger = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Serverless CI/CD Demo",
+      version: 'v1.0',
+      timestamp: new Date()
+    }),
+  };
+};
